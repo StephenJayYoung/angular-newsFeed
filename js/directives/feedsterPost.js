bfcad9e108d7f3f38dafa113f5243cbcc6ffe9de
Stephen Young <stephenjayyoung@gmail.com>
2016-03-29 16:53:48 -0700

    working on exercise

---
+++ js/directives/feedsterPost.js
@@ -0,0 +1,5 @@
+<img class="avatar" ng-src="{{posts.author.avatar}}"  >
+<h3 class="author-name">{{posts.author.name}} </h3>
+<p class="comment-text"> {{posts.comment.text}}</p>
+<img class="comment-img" ng-src="{{posts.comment.img}}"  >
+
